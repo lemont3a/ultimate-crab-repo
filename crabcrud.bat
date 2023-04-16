c33c638a21bac08ef88636e0bd796b1b85945d44
lumo <52174272+lemont3a@users.noreply.github.com>
2023-04-16 22:46:19 +0100

    Add files via upload
    need to do more

---
+++ crabcrud.bat
@@ -0,0 +1,33 @@
+@echo off
+set /p foldername=Enter folder name for your crabby pics: 
+
+set urls="https://media.discordapp.net/attachments/674703125562785798/1069665132021551256/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085311307223420969/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085311087588683856/image.png" "https://media.discordapp.net/attachments/674703125562785798/1085310851260612689/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310787620454450/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310683706576906/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310563057401947/image.png" "https://media.discordapp.net/attachments/674703125562785798/1085310492777644184/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310449182052402/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310374707994715/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310335138943026/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310273222619238/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310199495151666/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310143362760826/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310096726298634/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310049443921970/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085310002274771106/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085309945773293618/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085309891020861456/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1085309836465541180/image.png"^
+ "https://cdn.discordapp.com/attachments/674703125562785798/1070317124763004998/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070317124763004998/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070317069826015332/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070317035927646288/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316993107996772/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316937533472788/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316889152159754/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316828502544536/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316790078517289/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316723099672636/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316681563476018/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316595446026280/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316547949731931/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316492882722826/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316436368662639/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316391149875340/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316344349823037/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316296710914199/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316245171327067/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316197809238056/image.png"^
+  "https://cdn.discordapp.com/attachments/674703125562785798/1070316152410095657/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316099104686222/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070316058340241428/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070315989851455529/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070315034430947418/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314941027991552/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314876712529930/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314765487972362/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314765487972362/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314636144037939/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314522310606879/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314423220195428/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314367830212638/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314312821919784/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314252184862750/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314199235964998/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314140310183967/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314098438439052/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070314051499987045/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070313997657714798/image.png" "https://cdn.discordapp.com/attachments/674703125562785798/1070313883832680499/image.png"^
+
+
+echo URLs to download: %urls%
+
+mkdir "%foldername%"/
+
+setlocal EnableDelayedExpansion
+set counter=0
+
+for %%i in (%urls%) do (
+    set /A counter+=1
+    set "url_!counter!=%%~i"
+)
+
+for /L %%i in (1,1,%counter%) do (
+    powershell -command "(New-Object System.Net.WebClient).DownloadFile('!url_%%i!', '%cd%\%foldername%\%%i.png')"
+    if %errorlevel% EQU 0 (
+        echo %%i.png downloaded successfully to %cd%\%foldername%
+    ) else (
+        echo failed to download %%i.png
+    )
+)
+
+REM second half of urls below. current issue: still only downloading the top urls.
+
+echo crab image download success!
+
